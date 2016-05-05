1ba2f9fe44d71df8f5cbeceff58e4086b46ab3ef
Jonathan Eyler-Werve <jonathan@eylerwerve.com>
2016-05-05 11:07:26 -0500

    add a User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160505160336) do
+ActiveRecord::Schema.define(version: 20160505160648) do
 
   create_table "bots", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20160505160336) do
   create_table "responses", force: :cascade do |t|
     t.string   "message"
     t.string   "string"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "tweet_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
