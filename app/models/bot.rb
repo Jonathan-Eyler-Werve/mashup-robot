eec7902b0f9b6de333cb0e7f6e4c3b824b554e36
Jonathan Eyler-Werve <jonathan@eylerwerve.com>
2016-05-05 13:10:55 -0500

    Bug fixes

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -3,16 +3,18 @@ class Bot < ActiveRecord::Base
   def self.search_words words
     CLIENT.search(words, lang: "en").first.text
   end
-end
 
-def self.respond name
-  "@" + name + " " + Response.order_by_rand.first.message
-end
 
-def self.find_user number, words
-  CLINET.search(words, lang: "en").take(number).each { |t|
-    User.create(name: t.user.screen.name, tweet_id: t.id.to_s)
-    CLIENT.update(Bot.respond(t.user.screen_name), in_reply_to_status_id: t.id)
-  }
-end
+  def self.respond name
+    name = "mashuprobot" # safe mode
+    "@" + name + " " + Response.order_by_rand.first.message
+  end
 
+  def self.find_user number, words
+    CLIENT.search(words, lang: "en").take(number).each { |t|
+      User.create(name: t.user.screen_name, tweet_id: t.id.to_s)
+      CLIENT.update(Bot.respond(t.user.screen_name), in_reply_to_status_id: t.id)
+    }
+  end
+
+end
