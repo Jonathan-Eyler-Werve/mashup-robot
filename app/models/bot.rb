a909d007b6c522444b958da68b4af8630bd0ff3a
Jonathan Eyler-Werve <jonathan@eylerwerve.com>
2016-05-05 11:46:46 -0500

    method to respond to user based on search

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -5,3 +5,14 @@ class Bot < ActiveRecord::Base
   end
 end
 
+def self.respond name
+  "@" + name + " " + Response.order_by_rand.first.message
+end
+
+def self.find_user number, words
+  CLINET.search(words, lang: "en").take(number).each { |t|
+    User.create(name: t.user.screen.name, tweet_id: t.id.to_s)
+    CLIENT.update(Bot.respond(t.user.screen_name), in_reply_to_status_id: t.id)
+  }
+end
+
